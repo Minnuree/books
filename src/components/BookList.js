e33630e67da13a4f568a01729c346e48564908b2
Minnure <gminnure@hotmail.com>
2024-01-29 15:58:37 +0300

    ilk deneme

---
+++ src/components/BookList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './BookList.css';
+import Book from "./Book";
+
+class BookList extends React.Component {
+
+    
+
+    render() {
+
+        const bookList = this.props.books.map(
+        (book, i) => {
+            return <Book book={book} key={i} />
+        }
+    )
+        return <div>{bookList}</div>
+    }
+}
+
+export default BookList;
